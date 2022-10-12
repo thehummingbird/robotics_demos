f7bed5a42c316fa444ea62ecc385eda6557d9f8b
thehummingbird <sharadmaheshwari19@gmail.com>
2022-10-12 21:33:36 +0200

    feat: add behavior tree simple pick demo

---
+++ behavior_trees/simple_pick_demo/bt_demo.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <chrono>
+#include "behaviortree_cpp_v3/action_node.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+using namespace std::chrono_literals;
+
+class ApproachObject : public BT::SyncActionNode
+{
+public:
+  explicit ApproachObject(const std::string &name) : BT::SyncActionNode(name, {})
+  {
+  }
+
+  BT::NodeStatus tick() override
+  {
+    std::cout << "Approach Object: " << this->name() << std::endl;
+
+    std::this_thread::sleep_for(5s);
+    return BT::NodeStatus::SUCCESS;
+  }
+};
+
+BT::NodeStatus CheckBattery()
+{
+  std::cout << "Battery OK" << std::endl;
+  return BT::NodeStatus::SUCCESS;
+}
+
+class GripperInterface
+{
+public:
+  GripperInterface() : _open(true) {}
+
+  BT::NodeStatus open()
+  {
+    _open = true;
+    std::cout << "Gripper open" << std::endl;
+    return BT::NodeStatus::SUCCESS;
+  }
+
+  BT::NodeStatus close()
+  {
+    _open = false;
+    std::cout << "Gripper close" << std::endl;
+    return BT::NodeStatus::SUCCESS;
+  }
+
+private:
+  bool _open;
+};
+
+int main()
+{
+  BT::BehaviorTreeFactory factory;
+
+  factory.registerNodeType<ApproachObject>("ApproachObject");
+
+  factory.registerSimpleCondition("CheckBattery", std::bind(CheckBattery));
+
+  GripperInterface gripper;
+
+  factory.registerSimpleAction(
+      "OpenGripper",
+      std::bind(&GripperInterface::open, &gripper));
+
+  factory.registerSimpleAction(
+      "CloseGripper",
+      std::bind(&GripperInterface::close, &gripper));
+
+  //Create Tree
+  auto tree = factory.createTreeFromFile("./../bt_tree.xml");
+
+  //execute the tree
+  tree.tickRoot();
+
+  return 0;
+}
